dbff997f0df3665494d66eb991cbad38e41fd551
Antonis Papaioannou <mrantonp@gmail.com>
2025-10-15 20:45:19 +0300

    Refactor authentication system by integrating Better Auth. Removed .env.example file, updated package dependencies, and modified Prisma schema to include User, Session, Account, Verification, and Project models. Added new API routes for authentication and created auth client for Better Auth integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-
+import Link from 'next/link'
+import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-slate-950 text-white'>
+      <h1>HomePage</h1>
+      <p>
+        Welcome to the home page
+      </p>
+      <button>
+        Click me
+      </button>
+      <Link href="/about">About</Link>
     </div>
-  );
+    
+  )
 }
